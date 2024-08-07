87a150dad8811e702c48fabaf8f7fd718403bb86
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-08-07 13:17:41 +0200

    merging file from branch Louis

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Trie un tableau d'entiers par sélection
+ * @array: Pointeur vers le tableau à trier
+ * @size: Nombre d'éléments dans le tableau
+ *
+ * Return: None
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t min_id;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		min_id = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_id])
+				min_id = j;
+		}
+
+		temp = array[min_id];
+		array[min_id] = array[i];
+		array[i] = temp;
+
+		print_array(array, size);
+	}
+}
